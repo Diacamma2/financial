8f804837123c78362ed6349ca925e8dd08eb006e
Laurent GAY <l.gay@sd-libre.fr>
2024-08-15 16:00:20 +0200

    correct for tests

--- diacamma/accounting/test_tools.py
+++ diacamma/accounting/test_tools.py
@@ -212,23 +212,27 @@ def add_entry(yearid, journalid, date_value, designation, serial_entry, closed=F
     return new_entry
 
 
-def fill_entries_fr(yearid):
+def fill_entries_fr(yearid, withyear=False):
 
     default_costaccounting()
     # cost id=1: dep=12.34 / rec=0.00 => res=-12.34
     # cost id=2: dep=258.02 / rec=70.64 => res=-187.38
+    if withyear:
+        currentyear = str(FiscalYear.objects.get(id=yearid).begin.year)
+    else:
+        currentyear = '2015'
 
-    _ = add_entry(yearid, 1, '2015-02-01', 'Report à nouveau', '-1|5|0|1250.380000|0|0|None|\n-2|2|0|1135.930000|0|0|None|\n-3|3|0|114.450000|0|0|None|', True)  # 1 2 3
-    entry2 = add_entry(yearid, 2, '2015-02-14', 'depense 1', '-1|12|0|63.940000|2|0|None|\n-2|4|4|63.940000|0|0|None|', True)  # 4 5 - cost 2
-    entry3 = add_entry(yearid, 4, '2015-02-15', 'regement depense 1', '-1|2|0|-63.940000|0|0|ch N°34543|\n-2|4|4|-63.940000|0|0|None|', True)  # 6 7
-    entry4 = add_entry(yearid, 2, '2015-02-13', 'depense 2', '-1|14|0|194.080000|2|0|None|\n-2|4|1|194.080000|0|0|None|')  # 8 9 - cost 2
-    entry5 = add_entry(yearid, 4, '2015-02-17', 'regement depense 2', '-1|3|0|-194.080000|0|0|ch N°34545|\n-2|4|1|-194.080000|0|0|None|')  # 10 11
-    _ = add_entry(yearid, 2, '2015-02-20', 'depense 3', '-1|11|0|78.240000|0|0|None|\n-2|4|2|78.240000|0|0|None|')  # 12 13
-    entry7 = add_entry(yearid, 3, '2015-02-21', 'vente 1', '-1|10|0|70.640000|2|0|None|\n-2|1|7|70.640000|0|0|None|', True)  # 14 15 - cost 2
-    entry8 = add_entry(yearid, 4, '2015-02-22', 'regement vente 1', '-1|2|0|70.640000|0|0|BP N°654321|\n-2|1|7|-70.640000|0|0|None|', True)  # 16 17
-    _ = add_entry(yearid, 3, '2015-02-21', 'vente 2', '-1|10|0|125.970000|0|0|None|\n-2|1|5|125.970000|0|0|None|', True)  # 18 19
-    _ = add_entry(yearid, 3, '2015-02-24', 'vente 3', '-1|10|0|34.010000|0|0|None|\n-2|1|4|34.010000|0|0|None|')  # 20 21
-    _ = add_entry(yearid, 5, '2015-02-20', 'Frais bancaire', '-1|2|0|-12.340000|0|0|None|\n-2|15|0|12.340000|1|0|None|', True)  # 22 23 - cost 1
+    _ = add_entry(yearid, 1, '%s-02-01' % currentyear, 'Report à nouveau', '-1|5|0|1250.380000|0|0|None|\n-2|2|0|1135.930000|0|0|None|\n-3|3|0|114.450000|0|0|None|', True)  # 1 2 3
+    entry2 = add_entry(yearid, 2, '%s-02-14' % currentyear, 'depense 1', '-1|12|0|63.940000|2|0|None|\n-2|4|4|63.940000|0|0|None|', True)  # 4 5 - cost 2
+    entry3 = add_entry(yearid, 4, '%s-02-15' % currentyear, 'regement depense 1', '-1|2|0|-63.940000|0|0|ch N°34543|\n-2|4|4|-63.940000|0|0|None|', True)  # 6 7
+    entry4 = add_entry(yearid, 2, '%s-02-13' % currentyear, 'depense 2', '-1|14|0|194.080000|2|0|None|\n-2|4|1|194.080000|0|0|None|')  # 8 9 - cost 2
+    entry5 = add_entry(yearid, 4, '%s-02-17' % currentyear, 'regement depense 2', '-1|3|0|-194.080000|0|0|ch N°34545|\n-2|4|1|-194.080000|0|0|None|')  # 10 11
+    _ = add_entry(yearid, 2, '%s-02-20' % currentyear, 'depense 3', '-1|11|0|78.240000|0|0|None|\n-2|4|2|78.240000|0|0|None|')  # 12 13
+    entry7 = add_entry(yearid, 3, '%s-02-21' % currentyear, 'vente 1', '-1|10|0|70.640000|2|0|None|\n-2|1|7|70.640000|0|0|None|', True)  # 14 15 - cost 2
+    entry8 = add_entry(yearid, 4, '%s-02-22' % currentyear, 'regement vente 1', '-1|2|0|70.640000|0|0|BP N°654321|\n-2|1|7|-70.640000|0|0|None|', True)  # 16 17
+    _ = add_entry(yearid, 3, '%s-02-21' % currentyear, 'vente 2', '-1|10|0|125.970000|0|0|None|\n-2|1|5|125.970000|0|0|None|', True)  # 18 19
+    _ = add_entry(yearid, 3, '%s-02-24' % currentyear, 'vente 3', '-1|10|0|34.010000|0|0|None|\n-2|1|4|34.010000|0|0|None|')  # 20 21
+    _ = add_entry(yearid, 5, '%s-02-20' % currentyear, 'Frais bancaire', '-1|2|0|-12.340000|0|0|None|\n-2|15|0|12.340000|1|0|None|', True)  # 22 23 - cost 1
     Budget.objects.create(year_id=yearid, code='701', amount=67.89)
     Budget.objects.create(year_id=yearid, code='707', amount=123.45)
     Budget.objects.create(year_id=yearid, code='601', amount=8.19)
